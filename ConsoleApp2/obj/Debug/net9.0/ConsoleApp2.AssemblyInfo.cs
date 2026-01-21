3248b452b968d52b03910d0d6d39b0b1262f751c
El Miri Martin <elmiri.martin@gmail.com>
2026-01-21 19:44:27 +0100

    Create Book

--- ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+036723a026db3a9f9b90d7c9befd978d1f429caa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
